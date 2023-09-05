60b16095feb5be1ff679c8706d5f491512bd9ec3
Kaung Thant Zin <86509255+KaungThantZin2021@users.noreply.github.com>
2023-09-06 00:59:27 +0630

    [Finish] store setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ $store.state.Todo.name }}</h1>
+    <h1>{{ $store.state.a.name }}</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  mounted() {
+    console.log(this.$store.state);
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import Vuex from "vuex";
+import Vue from 'vue';
+import Todo from "./modules/Todo";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    modules: { 
+        Todo,
+        a: {
+            state: {
+                name: 'hello from a module'
+            }
+        }
+    },
+});
